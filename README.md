6f5941c7ed4fcf1812cce134130094f36df0ef95
Nebulea <github@nebulea.dev>
2025-02-18 22:58:35 +0100

    feat: add colors to terminal

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 `export NIX_CONFIG="experimental-features = nix-command flakes"`
 `nix shell nixpkgs#home-manager`
 `make`
+
+# Tips
+
+## Configure terminal
+
+- Run `dconf watch /` and change your terminal configs with the GUI to see the config changes in real time
--- home.nix
+++ home.nix
@@ -1,14 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  dconf.settings = with lib.hm.gvariant; {
-    "org/gnome/desktop/background" = {
-      color-shading-type = "solid";
-      picture-options = "zoom";
-      picture-uri = "file://" + ./assets/wallpaper.jpg;
-    };
-  };
-
   home = {
     packages = with pkgs; [
       hello
@@ -51,9 +43,8 @@
     ];
 
     activation.chezmoi = lib.hm.dag.entryAfter ["installPackages"] ''
-      ${pkgs.chezmoi}/bin/chezmoi init --apply --branch NixOS Nebulea-dev
+      ${pkgs.chezmoi}/bin/chezmoi init --apply Nebulea-dev
     '';
-
 
     # This needs to actually be set to your username
     username = "nixian";
@@ -62,6 +53,51 @@
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
     stateVersion = "23.11";
+  };
+
+  dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file://" + ./assets/wallpaper.jpg;
+    };
+  };
+
+  programs.gnome-terminal = {
+    enable = true;
+    showMenubar = false;
+
+    profile.b1dcc9dd-5262-4d8d-a863-c897e6d979b9 = {
+      default = true;
+      visibleName = "nix-terminal-conf";
+
+      showScrollbar = true;
+      font = "RobotoMono Nerd Font 16";
+      
+      colors = {
+        palette = [
+          "#36283d"
+          "#71ADE9"
+          "#AB8CAE"
+          "#9EA0D3"
+          "#8BB8E9"
+          "#E1B4CE"
+          "#cedaeb"
+          "#9098a4"
+          "#71ADE9"
+          "#AB8CAE"
+          "#E59DB1"
+          "#9EA0D3"
+          "#8BB8E9"
+          "#E1B4CE"
+          "#cedaeb"
+          "#FFFEFE"
+        ];
+
+        backgroundColor = "#36283d";
+        foregroundColor = "#c7ccd1";
+      };
+    };
   };
 }
 
